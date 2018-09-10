5dd3ae3081aeb489abbf2e36678afedfb52afa01
James Johnstone <jamesjohnstone15@gmail.com>
2018-09-10 08:01:37 +0100

    Remove blank line

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,7 +4,6 @@ from scraper.parsers import get_parser, HTMLParser
 
 
 class TestParser(unittest.TestCase):
-
     def test_get_html_parser(self):
         content_type = 'text/html; charset=utf-8'
         Parser = get_parser(content_type)
